e8678eb78cc3afb832e7dd6a9d2f5df5d7a42fd9
Ryan-ChungKamChung <74559899+Ryan-ChungKamChung@users.noreply.github.com>
2021-01-21 15:56:15 -0500

    Create constants.py

---
+++ Lesson3/constants.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by Ryan Chung Kam Chung
+# Created in January 2021
+# Constants file for code.py
+
+
+# Pybadge screen is 10x8 sprites (16x16)
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+
+# Frames per second that the game will run on
+FPS = 60
