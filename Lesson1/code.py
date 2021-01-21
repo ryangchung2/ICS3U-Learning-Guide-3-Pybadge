09bba1d68786d0b6ee7c41fb2276a4f1a5ab512e
Ryan-ChungKamChung <74559899+Ryan-ChungKamChung@users.noreply.github.com>
2021-01-21 15:38:18 -0500

    Create code.py

---
+++ Lesson1/code.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Created by Ryan Chung Kam Chung
+# Created in January 2021
+# "Hello, World!" on a PyBadge
+
+
+# Libraries or modules that will help us write the actual game
+import ugame
+import stage
+
+
+def game_scene():
+    # Main game scene, where the main game will run
+
+    # Prints "Hello, World!"
+    print("Hello, World!")
+
+    # Will be used later on
+    while True:
+        pass
+
+
+# Makes this file run as the main file of the program, and runs game_scene()
+if __name__ == "__main__":
+    game_scene()
