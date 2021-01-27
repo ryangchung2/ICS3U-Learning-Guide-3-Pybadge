aa6dff2ef089d14f233c14e90a512b567ea2fe39
Ryan-ChungKamChung <74559899+Ryan-ChungKamChung@users.noreply.github.com>
2021-01-27 15:56:57 -0500

    Create constants.py

---
+++ Lesson9/constants.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# Created by Ryan Chung Kam Chung
+# Created in January 2021
+# Constants file for code.py
+
+
+# Pybadge screen is 10x8 sprites (16x16)
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+
+# Pybadge screen is 160x128 pixels
+SCREEN_X = 160
+SCREEN_Y = 128
+
+# Frames per second that the game will run on
+FPS = 60
+
+# How fast the sprites move
+SPRITE_MOVEMENT_SPEED = 1
+
+# Button state
+button_state = {
+    "button_up": "up",
+    "button_just_pressed": "just pressed",
+    "button_still_pressed": "still pressed",
+    "button_released": "released"
+}
+
+# Ghost constants
+TOTAL_NUMBER_OF_GHOSTS = 1
+
+# Sword constants
+SWORD_SPEED = 2
+TOTAL_NUMBER_OF_SWORD_HITS = 1
+
+# When a bullet goes back to "staging"
+OFF_SCREEN_X = -1 * SPRITE_SIZE
+OFF_SCREEN_Y = -1 * SPRITE_SIZE
+OFF_TOP_SCREEN = -1 * SPRITE_SIZE
+OFF_BOTTOM_SCREEN = SCREEN_Y + SPRITE_SIZE
