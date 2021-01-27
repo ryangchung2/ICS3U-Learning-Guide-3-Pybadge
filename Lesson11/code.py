90c184c48a464c35549d9c5b42d8b0f6a3f1dcfe
Ryan-ChungKamChung <74559899+Ryan-ChungKamChung@users.noreply.github.com>
2021-01-27 16:13:35 -0500

    Update code.py

--- Lesson11/code.py
+++ Lesson11/code.py
@@ -16,58 +16,6 @@ import time
 import constants
 
 
-def menu_scene():
-    # Menu scene
-
-    # IMAGE BANK
-    menu_image_bank = stage.Bank.from_bmp16("image_bank_1.bmp")
-
-    # BACKGROUND
-    # Sets background to the 0th image in the image bank, 10x8 grid
-    menu_background = stage.Grid(menu_image_bank, constants.SCREEN_GRID_X,
-                                 constants.SCREEN_GRID_Y)
-
-    # Sets the floor as the 1st image in the image bank, the walls are
-    # still going to be the 0th image
-    for x_location in range(1, constants.SCREEN_GRID_X - 1):
-        for y_location in range(1, constants.SCREEN_GRID_Y - 1):
-            tile_picked = 1
-            menu_background.tile(x_location, y_location, tile_picked)
-
-    # TEXT
-    text = []
-    text1 = stage.Text(width=29, height=12, font=None,
-                       palette=constants.PALETTE, buffer=None)
-    text1.move(32, 10)
-    text1.text("Ghost Dodge!")
-    text.append(text1)
-
-    text2 = stage.Text(width=29, height=12, font=None,
-                       palette=constants.PALETTE, buffer=None)
-    text2.move(35, 110)
-    text2.text("PRESS START")
-    text.append(text2)
-
-    # STAGE AND RENDER
-    # creates stage and sets it to 60fps
-    game = stage.Stage(ugame.display, constants.FPS)
-    # sets the layers of all sprites, in order
-    game.layers = text + [menu_background]
-    # renders all sprites, only once
-    game.render_block()
-
-    while True:
-        # get user input
-        keys = ugame.buttons.get_pressed()
-
-        # select start button
-        if keys & ugame.K_START != 0:
-            game_scene()
-
-        # update game logic
-        game.tick()
-
-
 def game_scene():
     # Main game scene
 
